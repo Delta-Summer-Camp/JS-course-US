34e08f8b20e0cc8d3251bac00e92cfea84538f4a
Dima <d.ablov@gmail.com>
2020-07-26 22:07:01 +0300

    Начинаем разработку игры

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-const out = document.getElementById('output');
-const input = document.getElementById('input');
+const square = document.getElementById('square');
 
 document.addEventListener('keydown', getKey);
 
 function getKey(event) {
-    let str = '';
-    if (event.shiftKey === true) str += 'Shift+';
-    if (event.ctrlKey === true) str += 'Ctrl+';
-    if (event.altKey === true) str += 'Alt+';
-    out.innerText = str +  event.code;
-    return false;
-}
-
-function run() {
-
-    out.innerText = 'Hello, World!';
-
+    let key = event.code;
 }
