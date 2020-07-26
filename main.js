6f184599856bf3c656400c7ad365f99ba2316b44
Dima <d.ablov@gmail.com>
2020-07-26 22:27:59 +0300

    Начинаем разработку игры

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const square = document.getElementById('square');
-
 document.addEventListener('keydown', getKey);
 
+const STEP = 10;
+
 function getKey(event) {
-    let key = event.code;
+    let x = square.offsetLeft;
+    let y = square.offsetTop;
+
+    let key = event.keyCode;
+
+    switch (key) {
+        case 37: // left
+            x -= STEP;
+            break;
+        case 38: // up
+            y -= STEP;
+            break;
+        case 39: // right
+            x += STEP;
+            break;
+        case 40: // down
+            y += STEP;
+            break;
+        default:
+            return false;
+    }
+
+    square.style.left = x + 'px';
+    square.style.top = y + 'px';
 }
