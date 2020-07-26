151c00f7293312921e43cbd67f059945d49d3591
Dima <d.ablov@gmail.com>
2020-07-26 22:48:52 +0300

    Начало положено

--- main.js
+++ main.js
@@ -26,6 +26,11 @@ function getKey(event) {
             return false;
     }
 
+    if (x < 0) x = 0;
+    else if (x > 750) x = 750;
+    if (y < 0) y = 0;
+    else if (y > 750) y = 750;
+
     square.style.left = x + 'px';
     square.style.top = y + 'px';
 }
