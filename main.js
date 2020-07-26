aae8805dc083ef6e3812ef1fca0d8e359a462b90
Dima <d.ablov@gmail.com>
2020-07-26 22:55:48 +0300

    Динамические размеры

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const square = document.getElementById('square');
 document.addEventListener('keydown', getKey);
 
 const STEP = 10;
+const GA_SIZE = 800;
+const SQ_SIZE = 40;
+
+function init() {
+    square.style.width = square.style.height = SQ_SIZE + 'px';
+    const ga = document.getElementById('gameArea');
+    ga.style.width = ga.style.height = GA_SIZE + 'px';
+}
 
 function getKey(event) {
     let x = square.offsetLeft;
@@ -27,9 +35,9 @@ function getKey(event) {
     }
 
     if (x < 0) x = 0;
-    else if (x > 750) x = 750;
+    else if (x > GA_SIZE - SQ_SIZE) x = GA_SIZE - SQ_SIZE;
     if (y < 0) y = 0;
-    else if (y > 750) y = 750;
+    else if (y > GA_SIZE - SQ_SIZE) y = GA_SIZE - SQ_SIZE;
 
     square.style.left = x + 'px';
     square.style.top = y + 'px';
